2ca53f75cffe93c83f7ceb6f6addb4a20f79664e
MhranEbraheem <61749759+MhranEbraheem@users.noreply.github.com>
2020-07-13 22:40:24 +0300

    Updates

---
+++ precache-manifest.d5d3c89e978fb370a21f53c44dfa6ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a13ce40c5691421450a65049c4353e",
+    "url": "/smartt/index.html"
+  },
+  {
+    "revision": "7a80191addfc5d2307cb",
+    "url": "/smartt/static/css/main.2cd0946e.chunk.css"
+  },
+  {
+    "revision": "b28196f723edfce108d9",
+    "url": "/smartt/static/js/2.a855146f.chunk.js"
+  },
+  {
+    "revision": "a51e1a732616a65fe21698cc04856fb1",
+    "url": "/smartt/static/js/2.a855146f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a80191addfc5d2307cb",
+    "url": "/smartt/static/js/main.f4f1ab67.chunk.js"
+  },
+  {
+    "revision": "4f3de01fb56087d9ebf0",
+    "url": "/smartt/static/js/runtime-main.87647f74.js"
+  },
+  {
+    "revision": "88194f237d6d3ae961cce923f760f82e",
+    "url": "/smartt/static/media/201.88194f23.png"
+  }
+]);
